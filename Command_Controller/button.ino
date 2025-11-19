02e60918753579df49f910d2686b8c8fd7b097ba
Isaac J <88879000+fnfplayer64@users.noreply.github.com>
2025-11-19 11:48:30 +1100

    fggffgbfgdfb

--- Command_Controller/button.ino
+++ Command_Controller/button.ino
@@ -1,3 +1,5 @@
 The 12 buttons on the controller are the a useful and onvienient part of the command controller. they are located right next to the touch pads on each side, one in the form of of face buttons, whitch the remaining 4 being a d-pad.
 
-The buttons will mainly take a backseat to the microphone, acting as sort of a second fiddle
+The buttons will mainly take a backseat to the microphone, acting as sort of a second fiddle, mainly for slow, low stress uses.
+
+The buttons will be large in size, so people with limited dexterity (like our patient)
