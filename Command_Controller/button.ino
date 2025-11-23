bf39844f63e51c0aecef2850a444a50fc5edddd2
Isaac J <88879000+fnfplayer64@users.noreply.github.com>
2025-11-24 09:28:30 +1100

    fa

--- Command_Controller/button.ino
+++ Command_Controller/button.ino
@@ -1,7 +1,47 @@
-The 12 buttons on the controller are the a useful and onvienient part of the command controller. they are located right next to the touch pads on each side, one in the form of of face buttons, whitch the remaining 4 being a d-pad.
+#include <Wire.h>
+#include <U8g2lib.h>
 
-The buttons will mainly take a backseat to the microphone, acting as sort of a second fiddle, mainly for slow, low stress uses.
+const int lightPin = A0;            
+const int threshold = 2000;         
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
-The buttons will be large in size, so people with limited dexterity (like our patient) can easily use it. 
+unsigned long lastBlinkMillis = 0;
+const unsigned long blinkInterval = 300; // ms
+bool blinkOn = false;
 
-The size alos benifits amputees thanks to the
+void setup() {
+  Serial.begin(9600);
+  pinMode(lightPin, INPUT);
+  u8g2.begin();
+  u8g2.setFont(u8g2_font_ncenB14_tr);
+}
+
+void loop() {
+  int raw = analogRead(lightPin); // 0-16383 on R4
+  Serial.print("light raw: ");
+  Serial.println(raw);
+
+  
+  bool triggered = (raw < threshold);
+
+  if (triggered) {
+    unsigned long now = millis();
+    if (now - lastBlinkMillis >= blinkInterval) {
+      lastBlinkMillis = now;
+      blinkOn = !blinkOn;
+    }
+
+    u8g2.clearBuffer();
+    if (blinkOn) {
+      u8g2.drawStr(0, 32, "test");
+    }
+    u8g2.sendBuffer();
+  } else {
+    
+    u8g2.clearBuffer();
+    u8g2.sendBuffer();
+    blinkOn = false;
+  }
+
+  delay(50);
+}
---
+++ Command_Controller/button.md
@@ -0,0 +1,7 @@
+The 12 buttons on the controller are the a useful and onvienient part of the command controller. they are located right next to the touch pads on each side, one in the form of of face buttons, whitch the remaining 4 being a d-pad.
+
+The buttons will mainly take a backseat to the microphone, acting as sort of a second fiddle, mainly for slow, low stress uses.
+
+The buttons will be large in size, so people with limited dexterity (like our patient) can easily use it. 
+
+The size alos benifits amputees thanks to the
