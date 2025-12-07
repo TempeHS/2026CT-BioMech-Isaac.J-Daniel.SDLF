dc04d36a9a6a578f539df11980b74b39babde0d7
Daniel SDLF <daniel.sosadelafuente@education.nsw.gov.au>
2025-12-08 09:40:18 +1100

    i made it work im so cool

--- Command_Controller/LED.ino
+++ Command_Controller/LED.ino
@@ -1,70 +1,46 @@
 #include <Arduino.h>
-#include <U8g2lib.h>
 #include <Wire.h>
+#include <U8g2lib.h>
 
-// Initialize display (SSD1306 compatible)
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+ // Initialize display (SSD1306 compatible)
+ U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+ bool logoShown = false;
 
 void setup(void) {
   u8g2.begin();
   Serial.begin(9600);
   Serial.println("OLED Display initialized");
 }
-
 void loop(void) {
-  u8g2.clearBuffer();                   // Clear internal memory
 
-  u8g2.setFont(u8g2_font_ncenB08_tr);   // Choose font
-  u8g2.drawStr(0, 15, "Hello World!");  // Write at (x=0, y=15)
+  // Powering on screen
+  u8g2.clearBuffer();
 
-  u8g2.setFont(u8g2_font_ncenB14_tr);   // Larger font
-  u8g2.drawStr(0, 40, "Arduino");
+  u8g2.setFont(u8g2_font_ncenB08_tr);
+  u8g2.drawStr(0, 15, "Powering on...");
 
-  u8g2.sendBuffer();                    // Transfer to display
+  u8g2.setFont(u8g2_font_ncenB14_tr);
+  u8g2.drawStr(0, 40, "Welcome!");
 
-  delay(2000);
+  u8g2.sendBuffer();
+  
+  delay(2000); // Show welcome for 2 seconds
+
+  u8g2.clearBuffer();
+
+  if (!logoShown) 
+  u8g2.clearBuffer();
+  // Draw a circle for the face
+  u8g2.drawCircle(64, 32, 20);
+  // Eyes
+  u8g2.drawDisc(56, 28, 2);
+  u8g2.drawDisc(72, 28, 2);
+  // Smile
+  u8g2.drawArc(64, 34, 12, 150, 240);
+  u8g2.sendBuffer();
+  logoShown = true;
+  delay(3000); // Show logo for 3 seconds
+  u8g2.clearBuffer();
+  u8g2.sendBuffer();
 }
-// #include <Arduino.h>
-// #include <Wire.h>
-// #include <U8g2lib.h>
-// 
-//  // Initialize display (SSD1306 compatible)
-//  U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
-// 
-// void OLEDsetup() {
-//   u8g2.begin();
-//   Serial.begin(9600);
-//   Serial.println("OLED Display initialized");
-// }
-// void LEDLoop() {
-// 
-//   // Powering on screen
-//   u8g2.clearBuffer();
-// 
-//   u8g2.setFont(u8g2_font_ncenB08_tr);
-//   u8g2.drawStr(0, 15, "Powering on...");
-// 
-//   u8g2.setFont(u8g2_font_ncenB14_tr);
-//   u8g2.drawStr(0, 40, "Welcome!");
-// 
-//   u8g2.sendBuffer();
-//   
-//   delay(2000); // Show welcome for 2 seconds
-// }
-//  if (!logoShown) {
-//  display.clear();
-//  // Draw a circle for the face
-//  display.drawCircle(64, 32, 20);
-//  // Eyes
-//  display.drawDisc(56, 28, 2);
-//  display.drawDisc(72, 28, 2);
-//  // Smile
-//  display.drawArc(64, 34, 12, 0, 180);
-//  display.sendBuffer();
-//  logoShown = true;
-//  delay(3000); // Show logo for 3 seconds
-//  display.clear();
-//  display.sendBuffer();
-//}
-//
-//}
--- Command_Controller/LED.md
+++ Command_Controller/LED.md
@@ -1,3 +1,1 @@
-the OLED screen is supposed to show general information about the device, such as it's battery, the updates on pairing mode and the powering on and off screen. It
- will also show the welcome screen.
-When the command controller is powered on, the OLED screen will display a "powering on... welcome" message. After it is powered on, this message will dissapear.
+the OLED screen is meant to show the powering on function of the device, a welcome screen and a simple logo, which in this case is just a simple smiley face
